3bdcbbe70a236c05b10a2ed3231fbb7de904cb85
cab13e <cab13e@acu.edu>
2017-02-06 19:21:07 -0600

    Database fix

--- addArticle.php
+++ addArticle.php
@@ -6,30 +6,11 @@ if ($link->connect_errno) {
     printf("Connect failed: %s\n", $link->connect_error);
     exit();
 }
-/**************************
-*
-* Database interactions
-*
-***************************/
-
-$loggedin = false;
-if(isset($_COOKIE["AppName"]))
-{
-	$name = $_COOKIE["AppName"];
-	$cryptedCookie = $_COOKIE[$name];
-	$cryptedName = crypt($name,"ilovetacos");
-	if($cryptedCookie == $cryptedName)
-		$loggedin = true;
-}
-else
-	$action = "none";
 
 if(isset($_REQUEST["action"]))
 	$action = $_REQUEST["action"];
 else
 	$action = "none";
-
-$message = "";
 
 if($action == "addArticle")
 {
@@ -41,6 +22,13 @@ if($action == "addArticle")
     $approved = 0;
 
     $result = $link->query("INSERT INTO pending (title, date, story, submitted_by, approved_by, approved) VALUES ('$title', '$date', '$story', '$author', '$approvedby', '$approved')");
+    if(!$result)
+    {
+        die('Can\'t query pending because: ' . $link->error);
+        echo ("<h2> Bad </h2>");
+    }
+    else
+        echo("<h2> submitted </h2>");
 }
 
 
@@ -67,13 +55,13 @@ if($action == "addArticle")
 	</header>
        <div class="two-thirds1 column" id="main">
             <h2>Add an article: </h2>
-            <form method="POST" action="home.php" name="addArticle">
+            <form method="POST" action="addArticle.php" name="addArticle">
                 Title: <input type="text" name="title" id="title" /> <br/>
                 Author: <input type="text" name="submitted_by" id="submitted_by" /> <br/>
                 <label for="content" class="col-sm-2 control-label">Content: </label> <br/>
                 <textarea rows="5" name="story" id="story"></textarea>
                 <input type="hidden" name="action" value="addArticle" /> 
-                <input type="Submit" value="Go" onClick="confirm" />
+                <input type="Submit" value="Go" />
             </form>
         </div>
 
